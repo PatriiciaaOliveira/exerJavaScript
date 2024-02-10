1962c3d699bb16e87f5612b094c93f7e2d685e75
PATRICIA RODRIGUES OLIVEIRA <157825841+PatriiciaaOliveira@users.noreply.github.com>
2024-02-10 16:35:16 -0300

    ok

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function tabuada() {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
-            tab.appendchild(item)
+            tab.appendChild(item)
             c++
         }
     }
